08e92c43ca711360995c9bb140b9d1031edd0e7d
Jacob <desplome@hotmail.com>
2015-05-19 13:19:26 +0200

    add site controller, add home_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/' => 'trainer#home'
+  get '/' => 'site#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
