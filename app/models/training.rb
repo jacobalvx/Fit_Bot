45e4b276108056947b9e83ba2920c98ff77b0be0
Jacob <desplome@hotmail.com>
2015-05-20 09:00:31 +0200

    Relations habtm ok

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,5 +1,4 @@
 class Training < ActiveRecord::Base
-	belongs_to :trainer, class_name: "User"
-	has_and_belongs_to_many :athletes, :class_name => "User", :join_table => "athlete_trainings", :association_foreign_key => "athlete_id"
-
-end
+	belongs_to :trainer, class_name: "User", foreign_key: "trainer_id"
+	has_and_belongs_to_many :athletes, :class_name => "User", :join_table => "athlete_trainings", :foreing_key => "athlete_id"
+end	
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	 validates :role, inclusion: { in: %w(trainer athlete)}
 	 validates :role, :password, :email, presence: true
-	 has_and_belongs_to_many :trainings, :class_name => "Training", :join_table => "athlete_trainings", :association_foreign_key => "training_id"
+
+	 has_many :trainings
+	 has_and_belongs_to_many :trainings, :class_name => "Training", :join_table => "athlete_trainings", :foreign_key => "training_id"
 end
